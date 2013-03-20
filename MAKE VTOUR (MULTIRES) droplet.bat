dfdf747427a595fa43208e48d62109f4b0e6039a
Dmitry Monastyrskiy <dlm@aaanet.ru>
2013-03-20 15:52:51 +0400

    Доработка заводского скина
    - Добавлен логотип
    - добавлен плагин editor
    - авторотация включена по умолчанию

--- MAKE VTOUR (MULTIRES) droplet.bat
+++ MAKE VTOUR (MULTIRES) droplet.bat
@@ -5,6 +5,12 @@ IF "%~1" == "" GOTO ERROR
 IF NOT EXIST "%~1" GOTO ERROR
 
 "%~dp0\kmakemultires" "%~dp0\templates\vtour-multires.config" %*
+
+echo Making html files
+FOR %%a IN (%*) DO copy "%~p1\vtour\tour.html" "%~p1\vtour\%%~na.html"
+
+FOR %%a IN (%*) DO "%~dp0\replace.exe" "%~p1\vtour\%%~na.html"
+
 GOTO DONE
 
 :ERROR
